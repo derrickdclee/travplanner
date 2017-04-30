02f2c4d2fb0c35765a98fb3ba924c44a040bd229
Derrick Lee <stjohndlee@gmail.com>
2017-04-30 19:32:46 +0000

    please work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     has_many :users, through: :memberships
-    has_many :memberships, depedent: :destroy
+    has_many :memberships, dependent: :destroy
     has_many :invites, dependent: :destroy
     has_many :traveldays, dependent: :destroy
 end
