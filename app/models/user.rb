ecea7cb9e408763c5daec0bc3c235605d6637d3d
Derrick Lee <stjohndlee@gmail.com>
2017-04-30 18:56:16 +0000

    could it be the dependent issue?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts, through: :memberships
-  has_many :memberships
-  has_many :invitations, class_name: 'Invite', foreign_key: 'recipient_id'
-  has_many :sent_invites, class_name: 'Invite', foreign_key: 'sender_id'
+  has_many :posts, through: :memberships, dependent: :destroy
+  has_many :memberships, dependent: :destroy
+  has_many :invitations, class_name: 'Invite', foreign_key: 'recipient_id', dependent: :destroy
+  has_many :sent_invites, class_name: 'Invite', foreign_key: 'sender_id', dependent: :destroy
 end
