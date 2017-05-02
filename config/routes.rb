ee595d2d81690656c57106e9958988eca6c38b1c
Derrick Lee <stjohndlee@gmail.com>
2017-05-02 02:24:02 +0000

    added background image + other styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :traveldays
   end
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', 
+  sessions: 'users/sessions' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
